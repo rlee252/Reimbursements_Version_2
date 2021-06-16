5debc23cf631becd049d05b0aefb0c09fb87e9b4
hpatel183 <hmpatel183@outlook.com>
2021-06-16 12:05:54 -0500

    Add user Model

--- src/main/java/com/revature/app/model/User.java
+++ src/main/java/com/revature/app/model/User.java
@@ -1,5 +1,47 @@
 package com.revature.app.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="user", uniqueConstraints = @UniqueConstraint(columnNames = {
+		"username", "email"
+}))
+@Data @AllArgsConstructor @NoArgsConstructor
 public class User {
 
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@JoinColumn(name = "user_id")
+	int userId;
+	
+	@Column(name = "username")
+	String username;
+	
+	@Column(name = "password")
+	String password;
+	
+	@Column(name = "firstname")
+	String firstname;
+	
+	@Column(name = "lastname")
+	String lastname;
+	
+	@Column(name = "email")
+	String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "role_id", nullable = false)
+	UserRole role;
 }
--- src/main/java/com/revature/app/model/UserRole.java
+++ src/main/java/com/revature/app/model/UserRole.java
@@ -1,5 +1,27 @@
 package com.revature.app.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="ERS_USER_ROLES")
+@Data @AllArgsConstructor @NoArgsConstructor
 public class UserRole {
 
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@JoinColumn(name = "role_id")
+	int roleId;
+	
+	@Column(name = "role")
+	String roleName;
 }
